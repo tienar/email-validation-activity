d25b0238edfff61911bf250b23aded966a445512
tienar <62941100+tienar@users.noreply.github.com>
2021-02-01 14:52:40 -0500

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -63,6 +63,16 @@ define([
 
     function onGetEndpoints(endpoints) {
         console.log(endpoints);
-    }            
+    }
+    
+    function ValidateEmail(mail) 
+    {
+     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
+      {
+        return (true)
+      }
+        alert("You have entered an invalid email address!")
+        return (false)
+    }
 
 });
